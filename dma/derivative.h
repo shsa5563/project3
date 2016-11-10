602962ff5b02c3abeb90d6aa852d5a5fd536dbaa
shsa5563 <shsa5563@colorado.edu>
2016-11-09 20:20:56 -0700

    14th Commit: Derivative.h

---
+++ dma/derivative.h
@@ -0,0 +1,13 @@
+/*
+ * derivative.h
+ *
+ *  Created on: Oct 12, 2016
+ *      Author: rohi2
+ */
+
+#ifndef SOURCES_DERIVATIVE_H_
+#define SOURCES_DERIVATIVE_H_
+
+#include <MKL25Z4.h>
+
+#endif /* SOURCES_DERIVATIVE_H_ */
